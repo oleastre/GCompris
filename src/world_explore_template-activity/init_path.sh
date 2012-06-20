f2beaa73457d87b6569609b4ba7114cf1f0f00c0
Beth Hadley <bhadley@mit.edu>
2012-06-20 00:20:37 -0400

    First draft of the world-exploration activity. Allows students to explore a theme around the world, such as music, landmarks, languages, etc. provides easy interface for developers to enter content could use additional features to make the game more copmlex

---
+++ src/world_explore_template-activity/init_path.sh
@@ -0,0 +1,8 @@
+# Set the different path for this activity
+# This is sourced by runit.sh
+path=$1
+activity=world_explore_template
+plugindir=$path/../boards/.libs
+pythonplugindir=$path 
+resourcedir=$path/resources/world_explore_template
+section="/experimental"
