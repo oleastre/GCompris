659f7ae386b0306c8d42652f179bc6598e0b0ede
Beth Hadley <bhadley@mit.edu>
2012-06-23 00:10:17 -0400

    - deleted explore_world_template-activity (I accidentally commited this folder earlier today. The correct activity is named world_explore_teamplate-activity. Sorry for the confusion.
    note_names-activity
    - replaced note_names-activity hard link with relative link

--- src/explore_world_template-activity/init_path.sh
+++
@@ -1,8 +0,0 @@
-# Set the different path for this activity
-# This is sourced by runit.sh
-path=$1
-activity=explore_world_template
-plugindir=$path/../boards/.libs
-pythonplugindir=$path 
-resourcedir=$path/resources/explore_world_template
-section="/experimental"
